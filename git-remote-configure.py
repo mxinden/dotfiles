d785d20b174c8b3970526672c07e26887012042d
Max Inden <mail@max-inden.de>
2023-01-17 17:11:47 +0100

    fix(git): Remove print

--- git-remote-configure.py
+++ git-remote-configure.py
@@ -10,7 +10,5 @@ project_name = os.path.basename(os.getcwd())
 https = (f'https://github.com/{username}/{project_name}.git')
 ssh = (f'git@github.com:{username}/{project_name}.git')
 
-print(https, ssh)
-
 subprocess.run(["git", "remote", "add", username, https])
 subprocess.run(["git", "remote", "set-url", "--push", username, ssh])
